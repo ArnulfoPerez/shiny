db60d1808dd2b6f56f058f18a1b526124f335a35
ArnulfoPerez <arnulfo.perez.p@gmail.com>
2020-05-24 17:25:10 -0500

    added readme and deleted extra code not used

--- app.R
+++ app.R
@@ -111,8 +111,7 @@ ui <- dashboardPage(
     dashboardSidebar(
         sidebarMenu(
             menuItem("Histogramas", tabName = "dashboard", icon = icon("dashboard")),
-            menuItem("Widgets", tabName = "widgets", icon = icon("th")),
-            menuItem("leaflet", tabName = "leaflet", icon = icon("th")),
+            menuItem("Read me", tabName = "widgets", icon = icon("th")),
             menuItem("México Pop.", tabName = "MéxicoPop", icon = icon("th")),
             menuItem("México Covid", tabName = "MéxicoCovid", icon = icon("th")),
             menuItem("Center Covid", tabName = "CenterCovid", icon = icon("th")),
@@ -136,38 +135,7 @@ ui <- dashboardPage(
             
             # Second tab content
             tabItem(tabName = "widgets",
-                    fluidRow(
-                        
-                        column(3, wellPanel(
-                            selectInput("input_type", "Input type",
-                                        c("slider", "text", "numeric", "checkbox",
-                                          "checkboxGroup", "radioButtons", "selectInput",
-                                          "selectInput (multi)", "date", "daterange"
-                                        )
-                            )
-                        )),
-                        
-                        column(3, wellPanel(
-                            # This outputs the dynamic UI component
-                            uiOutput("ui")
-                        )),
-                        
-                        column(3,
-                               tags$p("Input type:"),
-                               verbatimTextOutput("input_type_text"),
-                               tags$p("Dynamic input value:"),
-                               verbatimTextOutput("dynamic_value")
-                        )
-                    )
-            ),
-            
-            # third tab content
-            tabItem(tabName = "leaflet",
-                    fluidPage(
-                        leafletOutput("mymap"),
-                        p(),
-                        actionButton("recalc", "New points")
-                    )
+                    includeMarkdown("README.md")
             ),
             
             # fourth tab content
@@ -216,66 +184,6 @@ server <- function(input, output) {
     
     output$plot2 <- renderPlot({
         hist(df$SEXO,main = "Histograma por sexo",xlab = "Sexo",ylab = "Frecuencia")
-    })
-    
-    output$ui <- renderUI({
-        if (is.null(input$input_type))
-            return()
-        
-        # Depending on input$input_type, we'll generate a different
-        # UI component and send it to the client.
-        switch(input$input_type,
-               "slider" = sliderInput("dynamic", "Dynamic",
-                                      min = 1, max = 20, value = 10),
-               "text" = textInput("dynamic", "Dynamic",
-                                  value = "starting value"),
-               "numeric" =  numericInput("dynamic", "Dynamic",
-                                         value = 12),
-               "checkbox" = checkboxInput("dynamic", "Dynamic",
-                                          value = TRUE),
-               "checkboxGroup" = checkboxGroupInput("dynamic", "Dynamic",
-                                                    choices = c("Option 1" = "option1",
-                                                                "Option 2" = "option2"),
-                                                    selected = "option2"
-               ),
-               "radioButtons" = radioButtons("dynamic", "Dynamic",
-                                             choices = c("Option 1" = "option1",
-                                                         "Option 2" = "option2"),
-                                             selected = "option2"
-               ),
-               "selectInput" = selectInput("dynamic", "Dynamic",
-                                           choices = c("Option 1" = "option1",
-                                                       "Option 2" = "option2"),
-                                           selected = "option2"
-               ),
-               "selectInput (multi)" = selectInput("dynamic", "Dynamic",
-                                                   choices = c("Option 1" = "option1",
-                                                               "Option 2" = "option2"),
-                                                   selected = c("option1", "option2"),
-                                                   multiple = TRUE
-               ),
-               "date" = dateInput("dynamic", "Dynamic"),
-               "daterange" = dateRangeInput("dynamic", "Dynamic")
-        )
-    })
-    
-    output$input_type_text <- renderText({
-        input$input_type
-    })
-    
-    output$dynamic_value <- renderPrint({
-        str(input$dynamic)
-    })
-    points <- eventReactive(input$recalc, {
-        cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-    }, ignoreNULL = FALSE)
-    
-    output$mymap <- renderLeaflet({
-        leaflet() %>%
-            addProviderTiles(providers$Stamen.TonerLite,
-                             options = providerTileOptions(noWrap = TRUE)
-            ) %>%
-            addMarkers(data = points())
     })
     
     output$mexicoPop <- renderPlot({
